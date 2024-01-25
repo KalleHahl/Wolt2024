49accf44e85693d255c2af1b9f58746e930fd9b4
Kalle Hahl <kalle.hahl@hotmail.com>
2024-01-25 16:26:19 +0200

    added stricter validation to models and changed constants in fee calculator

--- src/models/order.py
+++ src/models/order.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class OrderInfo(BaseModel):
-    cart_value: int
-    delivery_distance: int
-    number_of_items: int
-    time: str
+    cart_value: int = Field(..., ge=0)
+    delivery_distance: int = Field(..., ge=0)
+    number_of_items: int = Field(..., ge=0)
+    time: str  # = Field(pattern="^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
 
 
 class OrderFeeResponse(BaseModel):
-    delivery_fee: int
+    delivery_fee: int = Field(..., ge=0)
--- src/services/order_fee_calculator.py
+++ src/services/order_fee_calculator.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from enum import Enum
 
 
-class FeeCalculatorConstants(Enum):
+class Consts(Enum):
     BASE_FEE = 200
     BASE_FEE_THRESHOLD = 1000
     MAX_FEE = 1500
@@ -25,68 +25,60 @@ class FeeCalculator:
     def calculate_fee(
         self, cart_value: int, distance: int, items: int, time: str
     ) -> int:
-        if cart_value >= FeeCalculatorConstants.FREE_DELIVERY.value:
+        if cart_value >= Consts.FREE_DELIVERY.value:
             return 0
         cart_fee = self._cart_value(cart_value)
         distance_fee = self._distance_fee(distance)
+        print(distance_fee)
         item_fee = self._item_fee(items)
         fee = distance_fee + item_fee + cart_fee
         parsed_time = self._parse_time(time)
         fee = (
-            int(fee * FeeCalculatorConstants.RUSH_HOUR_MULTIPLIER.value)
+            int(fee * Consts.RUSH_HOUR_MULTIPLIER.value)
             if self._is_friday_rush(parsed_time)
             else fee
         )
-        return min(fee, FeeCalculatorConstants.MAX_FEE.value)
+        return min(fee, Consts.MAX_FEE.value)
 
     def _cart_value(self, cart_value: int) -> int:
-        if cart_value >= FeeCalculatorConstants.CART_VALUE_THRESHOLD.value:
+        if cart_value >= Consts.CART_VALUE_THRESHOLD.value:
             return 0
-        return FeeCalculatorConstants.CART_VALUE_THRESHOLD.value - cart_value
+        return Consts.CART_VALUE_THRESHOLD.value - cart_value
 
     def _distance_fee(self, distance: int) -> int:
-        if distance <= FeeCalculatorConstants.BASE_FEE_THRESHOLD.value:
-            return FeeCalculatorConstants.BASE_FEE.value
+        if distance <= Consts.BASE_FEE_THRESHOLD.value:
+            return Consts.BASE_FEE.value
 
         return (
-            FeeCalculatorConstants.BASE_FEE.value
+            Consts.BASE_FEE.value
             + math.ceil(
-                (distance - FeeCalculatorConstants.BASE_FEE_THRESHOLD.value)
-                / FeeCalculatorConstants.ADDITIONAL_DISTANCE_INTERVAL.value
+                (distance - Consts.BASE_FEE_THRESHOLD.value)
+                / Consts.ADDITIONAL_DISTANCE_INTERVAL.value
             )
-            * FeeCalculatorConstants.ADDITIONAL_DISTANCE_CHARGE.value
+            * Consts.ADDITIONAL_DISTANCE_CHARGE.value
         )
 
     def _item_fee(self, items: int) -> int:
-        if items <= FeeCalculatorConstants.EXTRA_ITEM_CHARGE_THRESHOLD.value:
+        if items <= Consts.EXTRA_ITEM_CHARGE_THRESHOLD.value:
             return 0
 
-        extra_item_charge_multiplier = (
-            items - FeeCalculatorConstants.EXTRA_ITEM_CHARGE_THRESHOLD.value
-        )
+        extra_item_charge_multiplier = items - Consts.EXTRA_ITEM_CHARGE_THRESHOLD.value
 
-        if items <= FeeCalculatorConstants.BULK_CHARGE_THRESHOLD.value:
-            return (
-                extra_item_charge_multiplier
-                * FeeCalculatorConstants.EXTRA_ITEM_CHARGE.value
-            )
+        if items <= Consts.BULK_CHARGE_THRESHOLD.value:
+            return extra_item_charge_multiplier * Consts.EXTRA_ITEM_CHARGE.value
 
         return (
-            extra_item_charge_multiplier
-            * FeeCalculatorConstants.EXTRA_ITEM_CHARGE.value
-            + FeeCalculatorConstants.BULK_CHARGE.value
+            extra_item_charge_multiplier * Consts.EXTRA_ITEM_CHARGE.value
+            + Consts.BULK_CHARGE.value
         )
 
     def _parse_time(self, time: str) -> datetime:
-        # TODO: some type of error if the time string isnt in the ISO
         return datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
 
     def _is_friday_rush(self, time: datetime) -> bool:
-        is_friday = time.weekday() == FeeCalculatorConstants.FRIDAY.value
+        is_friday = time.weekday() == Consts.FRIDAY.value
         is_rush_hour = (
-            FeeCalculatorConstants.RUSH_HOUR_START.value
-            <= time.hour
-            < FeeCalculatorConstants.RUSH_HOUR_END.value
+            Consts.RUSH_HOUR_START.value <= time.hour < Consts.RUSH_HOUR_END.value
         )
 
         if is_friday and is_rush_hour:
