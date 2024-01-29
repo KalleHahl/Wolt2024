58f1414873b39042f80df9ae6561e613eaf6c053
Kalle Hahl <kalle.hahl@hotmail.com>
2024-01-29 21:28:04 +0200

    added comments

--- src/models/order.py
+++ src/models/order.py
@@ -9,7 +9,8 @@ class OrderInfo(BaseModel):
     time: str
 
     @field_validator("time")
-    def validate_time_is_ISO(cls, time_str) -> datetime:
+    def validate_time_is_ISO(cls, time_str: str) -> datetime:
+        """Validator to check if time is UTC in ISO format and convert it to datetime object"""
         try:
             time = datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
             return time
